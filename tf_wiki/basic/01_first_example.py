50beddb486c44d8f3d8b83cc77c8fcdb27e9d4de
yanmk <yanmengk@foxmail.com>
2020-01-08 13:18:55 +0800

    简单粗暴TF2.0

---
+++ tf_wiki/basic/01_first_example.py
@@ -0,0 +1,65 @@
+import tensorflow as tf
+A = tf.constant([[1,2],[3,4]])
+B = tf.constant([[5,6],[7,8]])
+
+C= tf.matmul(A,B)
+print(C)
+
+
+# TensorFlow 2.0 版本中，Eager Execution 模式将成为默认模式，无需额外调用 tf.enable_eager_execution() 函数
+# （不过若要关闭 Eager Execution，则需调用 tf.compat.v1.disable_eager_execution() 函数）
+
+# 定义一个随机数（标量）
+random_float = tf.random.uniform(shape=())
+print(random_float)
+
+# 定义一个有2个元素的零向量
+zero_vector = tf.zeros(shape=(2))
+print(zero_vector)
+
+# 定义两个2×2的常量矩阵
+A = tf.constant([[1., 2.], [3., 4.]])
+B = tf.constant([[5., 6.], [7., 8.]])
+print(A)
+print(B)
+
+# 张量的重要属性是其形状、类型和值。可以通过张量的 shape 、 dtype 属性和 numpy() 方法获得。例如：
+print(A.dtype)
+print(A.shape)
+print(A.numpy())
+
+
+# TensorFlow 的大多数 API 函数会根据输入的值自动推断张量中元素的类型（一般默认为 tf.float32 ）。
+# 不过你也可以通过加入 dtype 参数来自行指定类型，
+# 例如 zero_vector = tf.zeros(shape=(2), dtype=tf.int32) 将使得张量中的元素类型均为整数。
+# 张量的 numpy() 方法是将张量的值转换为一个 NumPy 数组。
+
+C = tf.add(A, B)    # 计算矩阵A和B的和
+D = tf.matmul(A, B) # 计算矩阵A和B的乘积
+print(C)
+print(D)
+
+
+# 自动求导机制，求导数
+# 计算函数 y(x) = x^2 在 x = 3 时的导数：
+x = tf.Variable(initial_value=3.)
+# 变量Variable与普通张量的一个重要区别是其默认能够被TensorFlow 的自动求导机制所求导，
+# 因此往往被用于定义机器学习模型的参数。
+
+# tf.GradientTape() 是一个自动求导的记录器，在其中的变量和计算步骤都会被自动记录。
+with tf.GradientTape() as tape: # 在tf.GradientTape()的上下文内，所有计算步骤都会被记录以用于求导
+    y = tf.square(x)
+y_grad = tape.gradient(y,x) # 计算y关于x的导数
+print([y.numpy(),y_grad.numpy()]) # 结果为[9.0, 6.0]
+
+# 对多元函数求偏导数，以及对向量或矩阵求导
+X = tf.constant([[1.,2.],[3.,4.]])
+y = tf.constant([[1.],[2.]])
+w = tf.Variable(initial_value=[[1.],[2.]])
+b = tf.Variable(initial_value=1.)
+
+with tf.GradientTape() as tape:
+    L = 0.5*tf.reduce_sum(tf.square(tf.matmul(X, w) + b - y))
+w_grad,b_grad = tape.gradient(L,[w,b])
+print(L.numpy(), w_grad.numpy(), b_grad.numpy())
+
---
+++ tf_wiki/basic/02_linear_regression.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+
+import numpy as np
+
+X_raw = np.array([2013, 2014, 2015, 2016, 2017], dtype=np.float32) # 年份
+y_raw = np.array([12000, 14000, 15000, 16500, 17500], dtype=np.float32) # 房价
+
+# 预处理
+X = (X_raw - X_raw.min()) / (X_raw.max() - X_raw.min())
+y = (y_raw - y_raw.min()) / (y_raw.max() - y_raw.min())
+print(X)
+print(y)
+
+# Numpy下的线性回归
+# a, b = 0, 0
+#
+# num_epoch = 10000
+# learning_rate = 1e-3
+# for e in range(num_epoch):
+#     # 手动计算损失函数关于自变量（模型参数）的梯度
+#     y_pred = a * X + b
+#     print(y_pred)
+#     grad_a, grad_b = (y_pred - y).dot(X), (y_pred - y).sum()
+#
+#     # 更新参数
+#     a, b = a - learning_rate * grad_a, b - learning_rate * grad_b
+#
+# print(a, b)
+
+# TensorFlow下的线性回归
+X = tf.constant(X)
+y = tf.constant(y)
+
+a = tf.Variable(initial_value=0.)
+b = tf.Variable(initial_value=0.)
+variables = [a, b]
+
+num_epoch = 10000
+optimizer = tf.keras.optimizers.SGD(learning_rate=1e-3)
+for e in range(num_epoch):
+    # 使用tf.GradientTape()记录损失函数的梯度信息
+    with tf.GradientTape() as tape:
+        y_pred = a * X + b
+        loss = 0.5 * tf.reduce_sum(tf.square(y_pred-y))
+    # TensorFlow自动计算损失函数关于自变量（模型参数）的梯度
+    grads = tape.gradient(loss,variables)
+    # TensorFlow自动根据梯度更新参数
+    optimizer.apply_gradients(grads_and_vars=zip(grads,variables))
+
+print(variables[0].numpy(),variables[1].numpy())
+
+
+
+
+
+
